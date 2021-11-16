84868a6187dec7564ab19871d79fef580d221b81
AntonSheluho <grinfrend2@gmail.com>
2021-11-16 11:08:52 +0300

    I have do this but maybe it's not correct solution

--- task N_10/script.js
+++ task N_10/script.js
@@ -12,11 +12,25 @@ const color = {
 function randomColor(colors) {
 
     const arrColor = Object.keys(colors);
-    console.log(arrColor)
     const random = (n, m) => Math.floor(Math.random() * (m - n + 1)) + n;
 
-    for (let i of arrColor) {
-        console.log(arrColor[random(1, arrColor.length - 1)]);
+    let acc = [];
+    let b;
+     hi:
+    while (acc.length < arrColor.length) {
+        b = random(0, arrColor.length - 1);
+        if (!acc.includes(b)) {
+            acc[acc.length] = b;
+        }
+        if (acc.length == 8) {
+            break hi
+        }
+        
+    }
+
+    for (let i = 0; i < acc.length; i++) {
+        console.log(acc[i])
+        console.log(arrColor[acc[i]])
     }
 };
 
