f3176159690c097cba7a58772882ed04e5622d95
AntonSheluho <grinfrend2@gmail.com>
2021-11-16 13:00:16 +0300

    first try

---
+++ task N_13/script.js
@@ -0,0 +1,20 @@
+function DRINKS_HASH_FUNC() {
+    constructor() {
+
+    }
+    DRINKS_HASH_FUNC.prototype.addValue = function() {
+
+    }
+
+    DRINKS_HASH_FUNC.prototype.getValue = function() {
+
+    }
+
+    DRINKS_HASH_FUNC.prototype.deleteValue = function() {
+
+    }
+
+    DRINKS_HASH_FUNC.prototype.getKeys = function() {
+
+    }
+}
