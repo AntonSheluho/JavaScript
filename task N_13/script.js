9fb36a9533afeb78523759d24b84ac76d004fa4a
AntonSheluho <grinfrend2@gmail.com>
2021-11-17 15:53:24 +0300

    I did sixty percent

--- task N_13/script.js
+++ task N_13/script.js
@@ -1,42 +1,47 @@
-// Window.prototype.DrinksHashClass = function() {
-//     const drinks = {
+function HashStorageFunc(drink, nameDrink) {
+    const self = this;
+    self[drink] = nameDrink;
+    
 
-//     }
-//     DrinksHashClass.prototype.addValue = function() {
+    HashStorageFunc.prototype.addValue = function(key,value) {
+        self[key] = value;
+    }
 
-//     }
+    HashStorageFunc.prototype.getValue = function(key) {
+        console.log(self[key])
+        return self[key];
+    }
 
-//     DrinksHashClass.prototype.getValue = function() {
+    HashStorageFunc.prototype.deleteValue = function(key) {
+        const a = Object.entries(self);
+        let acc;
 
-//     }
+        for (let i = 0; i < a.length; i++) {
 
-//     DrinksHashClass.prototype.deleteValue = function() {
+            if (a[i][0] == key) {
+               acc = key;
+               delete self[acc];
+               console.log(self);
+               return true
+            }
+        }
 
-//     }
+        console.log(false);
+        return false
+    }
 
-//     DrinksHashClass.prototype.getKeys = function() {
-
-//     }
-// }
-
-function HashStorageFunc(drink, info) {
-
-    return {drink: info};
-
-    // addValue(key, value) {
-        
-    // }
-
-    // getValue() {
-
-    // }
-
-    // deleteValue() {
-
-    // }
-
-    // getKeys() {
-
-    // }
+    HashStorageFunc.prototype.getKeys = function() {
+        let acc = [];
+        for (let [key, value] of Object.entries(self)) {
+            acc[acc.length] = key;
+        }
+        return acc;
+    }
 }
+
+const drinkStorage = new HashStorageFunc('Budweiser', {
+    'type': 'beer',
+    'alcoholic': true,
+    'info': 'Budweiser - легкий, ароматный, свежий лагер в американском стиле. Сварено из лучшего ячменного солода и смеси премиальных сортов хмеля.'
+});
 
