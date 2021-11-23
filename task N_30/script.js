18d2d65405e92d9ba71574895bd9bd8161091474
AntonSheluho <grinfrend2@gmail.com>
2021-11-23 22:36:47 +0300

    I have make it but not all

--- task N_30/script.js
+++ task N_30/script.js
@@ -1,23 +1,51 @@
-let imgA = document.getElementById('imgA')
+const img = document.getElementsByTagName('img');
 
-imgA.onmousedown = function(event) {
-    console.log(event.pageY, event.pageX)
- 
-    document.body.append(imgA);
-    moveAt(event.pageX, event.pageY)
-
-    function moveAt(pageX, pageY) {
-        imgA.style.left = pageX - imgA.offsetWidth / 2 + 'px';
-        imgA.style.top = pageY - imgA.offsetHeight / 2 + 'px';
+function getEl() {
+  console.log(sayTrue(img))
+  if (sayTrue(img)) {
+    for (let i = 0; i < img.length; i++) {
+      if (event.target == img[i]) {
+        
+        console.log(`before start func`)
+        img[i].onmousedown = function(event) {
+          console.log('work 2');
+          getIndex(img[i])
+            let shiftY = event.clientY - img[i].getBoundingClientRect().top;
+            let shiftX = event.clientX - img[i].getBoundingClientRect().left;
+        
+            function move(event) {
+                img[i].style.top = event.pageY - shiftY + 'px';
+                img[i].style.left = event.pageX - shiftX + 'px';
+            }
+        
+            document.addEventListener('mousemove', move);
+        
+            img[i].onmouseup = function() {
+                document.removeEventListener('mousemove', move);
+           }
+        }
+        img[i].ondragstart = function() {
+          return false;
+        };
+      } else {
+        img[i].style.zIndex = 0 
+      }
+    }
   }
-  function onMouseMove(event) {
-    moveAt(event.pageX, event.pageY);
+  console.log(sayTrue(img))
+}
+
+document.addEventListener('mouseover', getEl);
+
+function getIndex(el) {
+  el.style.zIndex = 1
+}
+
+function sayTrue(el) {
+  for (let j = 0; j < el.length; j++) {
+    if (event.target == img[j]) {
+      return true;
+    };
   }
-  document.addEventListener('mousemove', onMouseMove);
-
-  imgA.onmouseup = function() {
-    document.removeEventListener('mousemove', onMouseMove);
-    imgA.onmouseup = null;
-  };
-
+  return false
 }
