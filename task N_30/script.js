a622add0303b660fac6a9d4d2066d8c4aae3b8e9
AntonSheluho <grinfrend2@gmail.com>
2021-11-24 12:50:15 +0300

    finish

--- task N_30/script.js
+++ task N_30/script.js
@@ -1,51 +1,56 @@
 const img = document.getElementsByTagName('img');
 
+// I made it because while zIndex one of them ten but other images have zIndex less all images don't increase they zIndex more ten. I don't know why but after ten all right.
+// The image which user taking has max zIndex + 1.
+for (let i = 0; i < img.length; i++) {
+  img[i].style.zIndex = 11
+}
+
+// Function which find image with max zIndex. But zIndex is constantly growing for all images. It isn't problem for this task but it's problem for big sita.
+// Here should be validation for big site which should return primary zIndex for other images.
+function findHighestZIndex() {
+    const el = document.getElementsByTagName('img');
+    let highest = 0;
+    for (let i = 0; i < el.length; i++)
+    {
+        let zIndex = el[i].style.zIndex;
+        if (zIndex > highest) {
+            highest = zIndex;
+        }
+    }
+    return highest;
+}
+
+// function which move any images 
 function getEl() {
-  console.log(sayTrue(img))
-  if (sayTrue(img)) {
+    let shiftY;
+    let shiftX;
+
     for (let i = 0; i < img.length; i++) {
-      if (event.target == img[i]) {
-        
-        console.log(`before start func`)
         img[i].onmousedown = function(event) {
-          console.log('work 2');
-          getIndex(img[i])
-            let shiftY = event.clientY - img[i].getBoundingClientRect().top;
-            let shiftX = event.clientX - img[i].getBoundingClientRect().left;
+            if (event.target == img[i]) {
+              img[i].style.zIndex = +findHighestZIndex() + 1;
+              img[i].style.cursor = 'none';
+              shiftY = event.clientY - img[i].getBoundingClientRect().top;
+              shiftX = event.clientX - img[i].getBoundingClientRect().left;
+            }
         
             function move(event) {
-                img[i].style.top = event.pageY - shiftY + 'px';
-                img[i].style.left = event.pageX - shiftX + 'px';
+                if (event.target == img[i]) {
+                    img[i].style.top = event.pageY - shiftY + 'px';
+                    img[i].style.left = event.pageX - shiftX + 'px';
+                }
             }
         
             document.addEventListener('mousemove', move);
         
             img[i].onmouseup = function() {
+                img[i].style.cursor = 'default'
                 document.removeEventListener('mousemove', move);
-           }
+            }
         }
-        img[i].ondragstart = function() {
-          return false;
-        };
-      } else {
-        img[i].style.zIndex = 0 
-      }
     }
-  }
-  console.log(sayTrue(img))
 }
 
+// Event Listener which look at when user push a mousedown 
 document.addEventListener('mouseover', getEl);
-
-function getIndex(el) {
-  el.style.zIndex = 1
-}
-
-function sayTrue(el) {
-  for (let j = 0; j < el.length; j++) {
-    if (event.target == img[j]) {
-      return true;
-    };
-  }
-  return false
-}
