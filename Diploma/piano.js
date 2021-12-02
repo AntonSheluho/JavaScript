a0fc9fe27a22c7aa5ecb9b9f416634534c0abff9
AntonSheluho <grinfrend2@gmail.com>
2021-12-02 18:31:50 +0300

    i made it. i don't know why i is not capital

--- Diploma/piano.js
+++ Diploma/piano.js
@@ -55,12 +55,13 @@ const keySee2 = document.createElement('div');
 const keyDo3 = document.createElement('div');
 const key = `
     width: 6.66%;
-    height: 80%;
+    height: 100%;
     border-radius: 5%;
     border: 1px solid black;
     display: flex;
     justify-content: center;
     align-items: center;
+    background-color: white;
 `;
 
 keyDo1.style.cssText = `${key}`;
@@ -82,26 +83,25 @@ keyDo3.style.cssText = `${key}`;
 piano.style.cssText = `
     width: 80%;
     height: 40%;
-    background-color: white;
     z-index: 2;
     display: flex;
     flex-direction: row;
 `;
 
 keyDo1.innerHTML = `<strong>A</strong> (До)`;
-keyRe1.innerHTML = `<strong>S</strong> (До)`;
-keyMy1.innerHTML = `<strong>D</strong> (До)`;
-keyFu1.innerHTML = `<strong>F</strong> (До)`;
-keySol1.innerHTML = `<strong>G</strong> (До)`;
-keyLea1.innerHTML = `<strong>H</strong> (До)`;
-keySee1.innerHTML = `<strong>J</strong> (До)`;
+keyRe1.innerHTML = `<strong>S</strong> (Ре)`;
+keyMy1.innerHTML = `<strong>D</strong> (Ми)`;
+keyFu1.innerHTML = `<strong>F</strong> (Фа)`;
+keySol1.innerHTML = `<strong>G</strong> (Соль)`;
+keyLea1.innerHTML = `<strong>H</strong> (Ля)`;
+keySee1.innerHTML = `<strong>J</strong> (Си)`;
 keyDo2.innerHTML = `<strong>K</strong> (До)`;
-keyRe2.innerHTML = `<strong>L</strong> (До)`;
-keyMy2.innerHTML = `<strong>:</strong> (До)`;
-keyFu2.innerHTML = `<strong>"</strong> (До)`;
-keySol2.innerHTML = `<strong>|</strong> (До)`;
-keyLea2.innerHTML = `<strong>4</strong> (До)`;
-keySee2.innerHTML = `<strong>5</strong> (До)`;
+keyRe2.innerHTML = `<strong>L</strong> (Ре)`;
+keyMy2.innerHTML = `<strong>:</strong> (Ми)`;
+keyFu2.innerHTML = `<strong>"</strong> (Фа)`;
+keySol2.innerHTML = `<strong>|</strong> (Соль)`;
+keyLea2.innerHTML = `<strong>4</strong> (Ля)`;
+keySee2.innerHTML = `<strong>5</strong> (Си)`;
 keyDo3.innerHTML = `<strong>6</strong> (До)`;
 
 body.insertAdjacentElement('afterbegin', bodyShadow);
@@ -123,6 +123,46 @@ piano.insertAdjacentElement('beforeend', keySol2);
 piano.insertAdjacentElement('beforeend', keyLea2);
 piano.insertAdjacentElement('beforeend', keySee2);
 piano.insertAdjacentElement('beforeend', keyDo3);
+
+addEventListener('keydown', (event) => {
+    switch (event.code) {
+        case 'KeyA': {keyDo1.style.backgroundColor = `red`; break};
+        case 'KeyS': {keyRe1.style.backgroundColor = `orange`; break};
+        case 'KeyD': {keyMy1.style.backgroundColor = `yellow`; break};
+        case 'KeyF': {keyFu1.style.backgroundColor = `green`; break};
+        case 'KeyG': {keySol1.style.backgroundColor = `blue`; break};
+        case 'KeyH': {keyLea1.style.backgroundColor = `indigo`; break};
+        case 'KeyJ': {keySee1.style.backgroundColor = `violet`; break};
+        case 'KeyK': {keyDo2.style.backgroundColor = `red`; break};
+        case 'KeyL': {keyRe2.style.backgroundColor = `orange`; break};
+        case 'Semicolon': {keyMy2.style.backgroundColor = `yellow`; break};
+        case 'Quote': {keyFu2.style.backgroundColor = `green`; break};
+        case 'Backslash': {keySol2.style.backgroundColor = `blue`; break};
+        case 'Numpad4': {keyLea2.style.backgroundColor = `indigo`; break};
+        case 'Numpad5': {keySee2.style.backgroundColor = `violet`; break};
+        case 'Numpad6': {keyDo3.style.backgroundColor = `red`; break};
+    }
+})
+
+addEventListener('keyup', (event) => {
+    switch (event.code) {
+        case 'KeyA': {keyDo1.style.backgroundColor = `white`; break}
+        case 'KeyS': {keyRe1.style.backgroundColor = `white`; break};
+        case 'KeyD': {keyMy1.style.backgroundColor = `white`; break};
+        case 'KeyF': {keyFu1.style.backgroundColor = `white`; break};
+        case 'KeyG': {keySol1.style.backgroundColor = `white`; break};
+        case 'KeyH': {keyLea1.style.backgroundColor = `white`; break};
+        case 'KeyJ': {keySee1.style.backgroundColor = `white`; break};
+        case 'KeyK': {keyDo2.style.backgroundColor = `white`; break};
+        case 'KeyL': {keyRe2.style.backgroundColor = `white`; break};
+        case 'Semicolon': {keyMy2.style.backgroundColor = `white`; break};
+        case 'Quote': {keyFu2.style.backgroundColor = `white`; break};
+        case 'Backslash': {keySol2.style.backgroundColor = `white`; break};
+        case 'Numpad4': {keyLea2.style.backgroundColor = `white`; break};
+        case 'Numpad5': {keySee2.style.backgroundColor = `white`; break};
+        case 'Numpad6': {keyDo3.style.backgroundColor = `white`; break};
+    }
+})
 
 
 
