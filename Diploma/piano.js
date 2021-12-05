8c3fbd819ed090d6db3bc99fe263bf06dc459fb8
AntonSheluho <grinfrend2@gmail.com>
2021-12-05 21:32:49 +0300

    I made black keys with animation

--- Diploma/piano.js
+++ Diploma/piano.js
@@ -60,7 +60,7 @@ const key = `
     border: 1px solid black;
     display: flex;
     justify-content: center;
-    align-items: center;
+    align-items: end;
     background-color: white;
 `;
 
@@ -86,6 +86,7 @@ piano.style.cssText = `
     z-index: 2;
     display: flex;
     flex-direction: row;
+    position: relative;
 `;
 
 keyDo1.innerHTML = `<strong>A</strong> (До)`;
@@ -140,42 +141,228 @@ const audioLya2 = new Audio('./audio/lya.mp3');
 const audioSi2 = new Audio('./audio/si.mp3');
 const audioDo3 = new Audio('./audio/do.mp3');
 
+const blackKeyC1 = document.createElement('div'); 
+const blackKeyD1 = document.createElement('div'); 
+const blackKeyF1 = document.createElement('div'); 
+const blackKeyG1 = document.createElement('div'); 
+const blackKeyA1 = document.createElement('div'); 
+const blackKeyC2 = document.createElement('div'); 
+const blackKeyD2 = document.createElement('div'); 
+const blackKeyF2 = document.createElement('div'); 
+const blackKeyG2 = document.createElement('div'); 
+const blackKeyA2 = document.createElement('div'); 
+const blackKey = `background-color: black; color: white; border-bottom: solid 0.8rem black; border-left: solid 0.3rem black;
+                border-right: solid 0rem black; position: absolute; width: 4%; height: 60%; top: 0; display: flex;
+                justify-content: center; align-items: end; transition-duration: 0.05s; font-size: 12px;`;
+
+blackKeyC1.style.cssText = `${blackKey} left: 4%;`; 
+blackKeyD1.style.cssText = `${blackKey} left: 10.8%;`; 
+blackKeyF1.style.cssText = `${blackKey} left: 24.1%;`; 
+blackKeyG1.style.cssText = `${blackKey} left: 30.8%;`; 
+blackKeyA1.style.cssText = `${blackKey} left: 37.5%;`; 
+blackKeyC2.style.cssText = `${blackKey} left: 50.9%;`; 
+blackKeyD2.style.cssText = `${blackKey} left: 57.6%;`; 
+blackKeyF2.style.cssText = `${blackKey} left: 71%;`; 
+blackKeyG2.style.cssText = `${blackKey} left: 77.7%;`; 
+blackKeyA2.style.cssText = `${blackKey} left: 84.4%;`; 
+
+blackKeyC1.innerHTML = `<strong>W</strong>(До#)`; 
+blackKeyD1.innerHTML = `<strong>E</strong>(Ре#)`;
+blackKeyF1.innerHTML = `<strong>T</strong>(Фа#)`;
+blackKeyG1.innerHTML = `<strong>Y</strong>(Соль#)`; 
+blackKeyA1.innerHTML = `<strong>U</strong>(Ля#)`; 
+blackKeyC2.innerHTML = `<strong>O</strong>(До#)`; 
+blackKeyD2.innerHTML = `<strong>P</strong>(Ре#)`; 
+blackKeyF2.innerHTML = `<strong>}</strong>(Фа#)`; 
+blackKeyG2.innerHTML = `<strong>7</strong>(Соль#)`; 
+blackKeyA2.innerHTML = `<strong>8</strong>(Ля#)`;
+
+piano.insertAdjacentElement('beforeend', blackKeyC1);
+piano.insertAdjacentElement('beforeend', blackKeyD1);
+piano.insertAdjacentElement('beforeend', blackKeyF1);
+piano.insertAdjacentElement('beforeend', blackKeyG1);
+piano.insertAdjacentElement('beforeend', blackKeyA1);
+piano.insertAdjacentElement('beforeend', blackKeyC2);
+piano.insertAdjacentElement('beforeend', blackKeyD2);
+piano.insertAdjacentElement('beforeend', blackKeyF2);
+piano.insertAdjacentElement('beforeend', blackKeyG2);
+piano.insertAdjacentElement('beforeend', blackKeyA2);
+
 addEventListener('keydown', (event) => {
     switch (event.code) {
         case 'KeyA': {keyDo1.style.backgroundColor = `red`; audioDo1.play(); break};
+        case 'KeyW': {
+            blackKeyC1.style.borderBottom = `solid 0rem black`; 
+            blackKeyC1.style.borderLeft = `solid 0rem black`; 
+            blackKeyC1.style.height = `63.5%`; 
+            audioDo2.play(); 
+            break
+        };
         case 'KeyS': {keyRe1.style.backgroundColor = `orange`; audioRe1.play(); break};
+        case 'KeyE': {
+            blackKeyD1.style.borderBottom = `solid 0rem black`; 
+            blackKeyD1.style.borderLeft = `solid 0rem black`; 
+            blackKeyD1.style.height = `63.5%`; 
+            audioRe1.play(); 
+            break
+        };
         case 'KeyD': {keyMi1.style.backgroundColor = `yellow`; audioMi1.play(); break};
         case 'KeyF': {keyFa1.style.backgroundColor = `green`; audioFa1.play(); break};
+        case 'KeyT': {
+            blackKeyF1.style.borderBottom = `solid 0rem black`; 
+            blackKeyF1.style.borderLeft = `solid 0rem black`; 
+            blackKeyF1.style.height = `63.5%`; 
+            audioFa1.play(); 
+            break
+        };
         case 'KeyG': {keySol1.style.backgroundColor = `blue`; audioSol1.play(); break};
+        case 'KeyY': {
+            blackKeyG1.style.borderBottom = `solid 0rem black`; 
+            blackKeyG1.style.borderLeft = `solid 0rem black`; 
+            blackKeyG1.style.height = `63.5%`; 
+            audioSol1.play(); 
+            break
+        };
         case 'KeyH': {keyLya1.style.backgroundColor = `indigo`; audioLya1.play(); break};
+        case 'KeyU': {
+            blackKeyA1.style.borderBottom = `solid 0rem black`; 
+            blackKeyA1.style.borderLeft = `solid 0rem black`; 
+            blackKeyA1.style.height = `63.5%`;
+            audioLya1.play(); 
+            break
+        };
         case 'KeyJ': {keySi1.style.backgroundColor = `violet`; audioSi1.play(); break};
         case 'KeyK': {keyDo2.style.backgroundColor = `red`; audioDo2.play(); break};
+        case 'KeyO': {
+            blackKeyC2.style.borderBottom = `solid 0rem black`; 
+            blackKeyC2.style.borderLeft = `solid 0rem black`; 
+            blackKeyC2.style.height = `63.5%`;
+            audioDo2.play(); 
+            break
+        };
         case 'KeyL': {keyRe2.style.backgroundColor = `orange`; audioRe2.play(); break};
+        case 'KeyP': {
+            blackKeyD2.style.borderBottom = `solid 0rem black`; 
+            blackKeyD2.style.borderLeft = `solid 0rem black`; 
+            blackKeyD2.style.height = `63.5%`;
+            audioRe2.play(); 
+            break
+        };
         case 'Semicolon': {keyMi2.style.backgroundColor = `yellow`; audioMi2.play(); break};
         case 'Quote': {keyFa2.style.backgroundColor = `green`; audioFa2.play(); break};
+        case 'BracketRight': {
+            blackKeyF2.style.borderBottom = `solid 0rem black`; 
+            blackKeyF2.style.borderLeft = `solid 0rem black`; 
+            blackKeyF2.style.height = `63.5%`; 
+            audioFa2.play(); 
+            break
+        };
         case 'Backslash': {keySol2.style.backgroundColor = `blue`; audioSol2.play(); break};
         case 'Numpad4': {keyLya2.style.backgroundColor = `indigo`; audioLya2.play(); break};
+        case 'Numpad7': {
+            blackKeyG2.style.borderBottom = `solid 0rem black`; 
+            blackKeyG2.style.borderLeft = `solid 0rem black`; 
+            blackKeyG2.style.height = `63.5%`; 
+            audioLya2.play(); 
+            break
+        };
         case 'Numpad5': {keySi2.style.backgroundColor = `violet`; audioSi2.play(); break};
+        case 'Numpad8': {
+            blackKeyA2.style.borderBottom = `solid 0rem black`; 
+            blackKeyA2.style.borderLeft = `solid 0rem black`; 
+            blackKeyA2.style.height = `63.5%`;
+            audioSi2.play(); 
+            break
+        };
         case 'Numpad6': {keyDo3.style.backgroundColor = `red`; audioDo3.play(); break};
     }
 })
 
 addEventListener('keyup', (event) => {
     switch (event.code) {
-        case 'KeyA': {keyDo1.style.backgroundColor = `white`; audioDo1.load(); break}
+        case 'KeyA': {keyDo1.style.backgroundColor = `white`; audioDo1.load(); break};
+        case 'KeyW': {
+            blackKeyC1.style.borderBottom = `solid 0.8rem black`; 
+            blackKeyC1.style.borderLeft = `solid 0.3rem black`; 
+            blackKeyC1.style.height = `60%`;
+            audioDo2.load(); 
+            break};
         case 'KeyS': {keyRe1.style.backgroundColor = `white`; audioRe1.load(); break};
+        case 'KeyE': {
+            blackKeyD1.style.borderBottom = `solid 0.8rem black`; 
+            blackKeyD1.style.borderLeft = `solid 0.3rem black`; 
+            blackKeyD1.style.height = `60%`; 
+            audioRe1.load(); 
+            break
+        };
         case 'KeyD': {keyMi1.style.backgroundColor = `white`; audioMi1.load(); break};
         case 'KeyF': {keyFa1.style.backgroundColor = `white`; audioFa1.load(); break};
+        case 'KeyT': {
+            blackKeyF1.style.borderBottom = `solid 0.8rem black`; 
+            blackKeyF1.style.borderLeft = `solid 0.3rem black`; 
+            blackKeyF1.style.height = `60%`; 
+            audioFa1.load(); 
+            break
+        };
         case 'KeyG': {keySol1.style.backgroundColor = `white`; audioSol1.load(); break};
+        case 'KeyY': {
+            blackKeyG1.style.borderBottom = `solid 0.8rem black`; 
+            blackKeyG1.style.borderLeft = `solid 0.3rem black`; 
+            blackKeyG1.style.height = `60%`;
+            audioSol1.load(); 
+            break
+        };
         case 'KeyH': {keyLya1.style.backgroundColor = `white`; audioLya1.load(); break};
+        case 'KeyU': {
+            blackKeyA1.style.borderBottom = `solid 0.8rem black`; 
+            blackKeyA1.style.borderLeft = `solid 0.3rem black`; 
+            blackKeyA1.style.height = `60%`;
+            audioLya1.load(); 
+            break
+        };
         case 'KeyJ': {keySi1.style.backgroundColor = `white`; audioSi1.load(); break};
         case 'KeyK': {keyDo2.style.backgroundColor = `white`; audioDo2.load(); break};
+        case 'KeyO': {
+            blackKeyC2.style.borderBottom = `solid 0.8rem black`; 
+            blackKeyC2.style.borderLeft = `solid 0.3rem black`; 
+            blackKeyC2.style.height = `60%`;
+            audioDo2.load(); 
+            break
+        };
         case 'KeyL': {keyRe2.style.backgroundColor = `white`; audioRe2.load(); break};
+        case 'KeyP': {
+            blackKeyD2.style.borderBottom = `solid 0.8rem black`; 
+            blackKeyD2.style.borderLeft = `solid 0.3rem black`; 
+            blackKeyD2.style.height = `60%`; 
+            audioRe2.load(); 
+            break
+        };
         case 'Semicolon': {keyMi2.style.backgroundColor = `white`; audioMi2.load(); break};
         case 'Quote': {keyFa2.style.backgroundColor = `white`; audioFa2.load(); break};
+        case 'BracketRight': {
+            blackKeyF2.style.borderBottom = `solid 0.8rem black`; 
+            blackKeyF2.style.borderLeft = `solid 0.3rem black`; 
+            blackKeyF2.style.height = `60%`; 
+            audioFa2.load(); 
+            break
+        };
         case 'Backslash': {keySol2.style.backgroundColor = `white`; audioSol2.load(); break};
         case 'Numpad4': {keyLya2.style.backgroundColor = `white`; audioLya2.load(); break};
+        case 'Numpad7': {
+            blackKeyG2.style.borderBottom = `solid 0.8rem black`; 
+            blackKeyG2.style.borderLeft = `solid 0.3rem black`; 
+            blackKeyG2.style.height = `60%`;
+            audioLya2.load(); 
+            break
+        };
         case 'Numpad5': {keySi2.style.backgroundColor = `white`; audioSi2.load(); break};
+        case 'Numpad8': {
+            blackKeyA2.style.borderBottom = `solid 0.8rem black`; 
+            blackKeyA2.style.borderLeft = `solid 0.3rem black`; 
+            blackKeyA2.style.height = `60%`;
+            audioSi2.load(); 
+            break
+        };
         case 'Numpad6': {keyDo3.style.backgroundColor = `white`; audioDo3.load(); break};
     }
 })
